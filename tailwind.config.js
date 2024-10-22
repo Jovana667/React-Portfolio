706ebe9be1a82e209e5320ab0209f64ec44fc1a3
Jovana667 <jovanavukovic4@hotmail.com>
2024-10-22 18:57:26 +1100

    corrected responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {
-        "navy-blue": "#1B2C4A",
-        "light-blue": "#8892b0",
-        "stripe-bg": "#e6e6e6",
-      },
-      fontFamily: {
-        serif: ["Georgia", "serif"],
-        sans: ["Helvetica", "Arial", "sans-serif"],
+      screens: {
+        xs: "480px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
       },
     },
   },
